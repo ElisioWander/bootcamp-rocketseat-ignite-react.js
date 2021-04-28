9796def210debf4d434c09326565de365d2c823d
elisioWander <elisio741@hotmail.com>
2021-04-28 04:18:20 -0300

    typescript-increment

--- react/01-github-explorer/src/components/RepositoryList.tsx
+++ react/01-github-explorer/src/components/RepositoryList.tsx
@@ -3,14 +3,16 @@ import { RepositoryItem } from './RepositoryItem'
 
 import '../styles/repositories.scss'
 
-const repository = {
-    name: 'umform',
-    description: 'Forms in React',
-    link: 'https://github/umform/umform'
+interface Repository {
+    name: string,
+    description: string,
+    html_url: string
 }
 
+
+
 export function RepositoryList() {
-    const [repositories, setRepositories] = useState([])
+    const [repositories, setRepositories] = useState<Repository[]>([])
 
     useEffect(() => {
         fetch('https://api.github.com/orgs/rocketseat/repos')
@@ -25,10 +27,9 @@ export function RepositoryList() {
             <h1>Lista de Repositório</h1>
 
             <ul>
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
-                <RepositoryItem repository={repository} />
+                { repositories.map(repository => {
+                    return <RepositoryItem key={repository.name} repository={repository} />
+                }) }
             </ul>
         </section>
     )
