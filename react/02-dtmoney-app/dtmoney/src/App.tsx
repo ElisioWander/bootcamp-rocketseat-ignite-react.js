a9f7ebe8e6e1e5cfb9cb64466c987e97743f9f86
elisioWander <elisio741@hotmail.com>
2021-05-09 00:50:25 -0300

    styled-components

--- react/02-dtmoney-app/dtmoney/src/App.tsx
+++ react/02-dtmoney-app/dtmoney/src/App.tsx
@@ -1,6 +1,15 @@
+import styled from 'styled-components'
+import { GlobalStyle } from './styles/global'
+
+const Title = styled.h1`
+  color: red;
+`
+
 export function App() {
   return (
     <div className="App">
+      <GlobalStyle />
+      <Title>Hello ReactJs</Title>
     </div>
   );
 }
