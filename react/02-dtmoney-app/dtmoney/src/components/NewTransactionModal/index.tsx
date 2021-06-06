e31020fb8e2f40fd3e3d716549ac6815153bbb99
elisioWander <elisio741@hotmail.com>
2021-06-06 03:05:51 -0300

    hook-useTransactions

--- react/02-dtmoney-app/dtmoney/src/components/NewTransactionModal/index.tsx
+++ react/02-dtmoney-app/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,6 +1,6 @@
 import { useState, useContext, FormEvent } from 'react'
 import Modal from 'react-modal'
-import { TransactionsContext } from '../../TransactionsContext' 
+import { useTransactions } from '../../hooks/useStransactions' 
 
 import { Container } from './styles'
 import { TransactionTypeContainer, RadioBox } from './styles'
@@ -16,7 +16,7 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
-    const { createTransaction } = useContext(TransactionsContext)
+    const { createTransaction } = useTransactions()
 
     const [title, setTitle] = useState('')
     const [amount, setAmount] = useState(0)
--- react/02-dtmoney-app/dtmoney/src/hooks/useStransactions.tsx
+++ react/02-dtmoney-app/dtmoney/src/hooks/useStransactions.tsx
@@ -1,5 +1,5 @@
-import { createContext, useEffect, useState, ReactNode } from 'react'
-import { api } from './services/api'
+import { createContext, useEffect, useState, useContext, ReactNode } from 'react'
+import { api } from '../services/api'
 
 interface Transaction {
     id: number;
@@ -23,7 +23,7 @@ interface TransactionContextData {
 
 
 
-export const TransactionsContext = createContext<TransactionContextData>(
+const TransactionsContext = createContext<TransactionContextData>(
    {} as TransactionContextData
 )
 
@@ -53,4 +53,10 @@ export function TransactionsProvider({children}: TransactionsProviderProps) {
             {children}
         </TransactionsContext.Provider>
     )
+}
+
+export function useTransactions() {
+    const context = useContext(TransactionsContext)
+
+    return context
 }
