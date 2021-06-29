4ece0297b76047df8c9db89d0ca7f9cc7c36ec54
elisioWander <elisio741@hotmail.com>
2021-06-28 23:53:24 -0300

    page-home-completed

--- react/ignews/src/Components/Header/index.tsx
+++ react/ignews/src/Components/Header/index.tsx
@@ -1,3 +1,5 @@
+import { SignInButton } from '../SignInButton'
+
 import styles from './styles.module.scss'
 
 export function Header() {
@@ -7,9 +9,11 @@ export function Header() {
         <img src="/images/logo.svg" alt="ig.news" />
 
         <nav>
-          <a href="#">Home</a>
+          <a className={styles.active} href="#">Home</a>
           <a href="#">Posts</a>
         </nav>
+
+        <SignInButton />
       </div>
     </header>
   )
--- react/ignews/src/pages/index.tsx
+++ react/ignews/src/pages/index.tsx
@@ -1,9 +1,29 @@
 import Head from "next/head";
+import { SubscribeButton } from "../Components/SubscribeButton/SubscribeButton";
+
+import styles from './home.module.scss'
 
 export default function Home() {
   return (
-    <Head>
-      <title>Início | ig.news</title>
-    </Head>
+    <>
+      <Head>
+        <title>Início | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+            <span>👏 Heym welcome</span>
+            <h1>News about the <span>React</span> World!</h1>
+            <p>
+              Get access to all publications <br/>
+              <span>for $9.90 month</span>
+            </p>
+
+            <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl Coding" />
+      </main>
+    </>
   )
 }
