e7a7d958a2310231ee2eeda98166f8b8f2dad4b3
elisioWander <elisio741@hotmail.com>
2021-07-21 04:19:42 -0300

    post-preview

--- react/ignews/src/Components/SubscribeButton/index.tsx
+++ react/ignews/src/Components/SubscribeButton/index.tsx
@@ -2,6 +2,8 @@ import { useSession, signIn } from "next-auth/client"
 import { api } from '../../services/api'
 import { getStripeJs } from "../../services/stripe-js"
 
+import { useRouter } from "next/router"
+
 import styles from "./styles.module.scss"
 
 interface SubscribeButtonProps {
@@ -10,10 +12,16 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [ session ] = useSession()
+  const router = useRouter()
 
   async function handleSubscribe() {
     if(!session) {
       signIn('github')
+      return
+    }
+
+    if(session.activeSubscription) {
+      router.push('/posts')
       return
     }
 
--- react/ignews/src/pages/posts/[slug].tsx
+++ react/ignews/src/pages/posts/[slug].tsx
@@ -2,8 +2,11 @@ import { GetServerSideProps } from 'next'
 import { getSession } from 'next-auth/client'
 import { RichText } from 'prismic-dom'
 import { getPrismicClient } from '../../services/prismic'
+import Head from 'next/head'
 
-type PostProps = {
+import styles from './post.module.scss'
+
+interface PostProps {
   post: {
     slug: string,
     title: string,
@@ -12,16 +15,42 @@ type PostProps = {
   }
 }
 
+
 export default function Post({ post }: PostProps) {
   return (
-    <h1>Testando Post page</h1>
+    <>
+      <Head>
+        <title>{post.title}</title>
+      </Head>
+
+      <main className={styles.container} >
+        <article className={styles.post}>
+          <h1>{post.title}</h1>
+          <time>{post.updatedAt}</time>
+          <div 
+            className={styles.postContent} 
+            dangerouslySetInnerHTML={{ __html: post.content }}
+          />
+        </article>
+      </main>
+    </>
   )
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req, params }) => {
   const session = await getSession({ req })
-
   const { slug } = params
+
+  console.log(session)
+
+  if(!session?.activeSubscription) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false
+      }
+    }
+  }
 
   const prismic = getPrismicClient(req)
 
