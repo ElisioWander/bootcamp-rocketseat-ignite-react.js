d83e49a63b8f6034fa218dd784fe86f505b164e1
elisioWander <elisio741@hotmail.com>
2021-07-22 02:58:42 -0300

    README Added

--- react/ignews/src/pages/posts/[slug].tsx
+++ react/ignews/src/pages/posts/[slug].tsx
@@ -41,17 +41,6 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
   const session = await getSession({ req })
   const { slug } = params
 
-  console.log(session)
-
-  if(!session?.activeSubscription) {
-    return {
-      redirect: {
-        destination: '/',
-        permanent: false
-      }
-    }
-  }
-
   const prismic = getPrismicClient(req)
 
   const response = await prismic.getByUID('post', String(slug), {})
@@ -65,6 +54,15 @@ export const getServerSideProps: GetServerSideProps = async ({ req, params }) =>
       month: 'long',
       year: 'numeric'
     })
+  }
+
+  if(!session?.activeSubscription) {
+    return {
+      redirect: {
+        destination: `/posts/preview/${post.slug}`,
+        permanent: false
+      }
+    }
   }
 
   return {
