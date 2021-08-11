782a6430c3a9cf6d444b000175fc524f34d42cdf
elisioWander <elisio741@hotmail.com>
2021-08-11 01:02:20 -0300

    sidebar-responsivity

--- react/dashgo/src/Components/Header/index.tsx
+++ react/dashgo/src/Components/Header/index.tsx
@@ -1,11 +1,16 @@
-import { Flex, useBreakpointValue } from '@chakra-ui/react'
+import { Flex, useBreakpointValue, IconButton, Icon } from '@chakra-ui/react'
 
 import { Logo } from '../Header/Logo'
 import { NotificationNav } from '../Header/NotificationNav'
 import { Profile } from '../Header/Profile'
 import { SearchBox } from '../Header/SearchBox'
+import { useSidebarDrawer } from '../../contexts/SidebarDrawerContext'
+
+import { RiMenuLine } from 'react-icons/ri'
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     md: true
@@ -16,6 +21,17 @@ export function Header() {
       as="header" w="100%" maxW={1480} h="20" mx="auto"
       mt="4" px="6" align="center"
     >
+      { !isWideVersion && (
+          <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          mr="2"
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+        ></IconButton>
+      ) }
+
       <Logo />
 
       { isWideVersion && 
