4108c4e4b87b1d7fe42d126bbf1f2717afe55fd8
ElisioWander <elisio741@hotmail.com>
2021-12-16 20:18:53 -0300

    sidebar-drawer-dashboard

--- react/dashgo/src/Components/Header/Index.tsx
+++ react/dashgo/src/Components/Header/Index.tsx
@@ -1,10 +1,14 @@
-import { Flex, useBreakpointValue} from '@chakra-ui/react'
+import { Flex, useBreakpointValue, IconButton, Icon} from '@chakra-ui/react'
+import { RiMenuLine } from 'react-icons/ri'
 import { Logo } from './Logo'
 import { Notification } from './Notification'
 import { Profile } from './Profile'
 import { SearchBox } from './SearchBox'
+import { useSidebarDrawer } from '../contexts/SidebarDrawerContext'
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer()
+
   const isWideVersion = useBreakpointValue({
     base: false,
     lg: true
@@ -21,6 +25,19 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWideVersion && (
+        <IconButton
+          aria-label="Open Navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          mr="2"
+          onClick={onOpen}
+        >
+
+        </IconButton>
+      )}
+
       <Logo />
       { isWideVersion && <SearchBox /> }
 
--- react/dashgo/src/Components/contexts/SidebarDrawerContext.tsx
+++ react/dashgo/src/Components/contexts/SidebarDrawerContext.tsx
@@ -1,5 +1,6 @@
-import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react"
-import { createContext, ReactNode, useContext } from "react"
+import { useDisclosure, UseDisclosureReturn } from "@chakra-ui/react";
+import { useRouter } from "next/dist/client/router";
+import { createContext, ReactNode, useContext, useEffect } from "react";
 
 interface SidebarDrawerProviderProps {
   children: ReactNode;
@@ -7,16 +8,22 @@ interface SidebarDrawerProviderProps {
 
 type SidebarDrawerContextData = UseDisclosureReturn
 
-const SidebarDrawerContext = createContext({} as SidebarDrawerContextData) 
+const SidebarDrawerContext = createContext({} as SidebarDrawerContextData)
 
 export function SidebarDrawerProvider({ children }: SidebarDrawerProviderProps) {
   const disclosure = useDisclosure()
+  const router = useRouter()
+
+  useEffect(() => {
+    disclosure.onClose()
+  }, [router.asPath])
+
 
   return (
     <SidebarDrawerContext.Provider value={disclosure} >
-      { children }
+      {children}
     </SidebarDrawerContext.Provider>
   )
 }
 
-export const useSidebarContext = () => useContext(SidebarDrawerContext) 
+export const useSidebarDrawer = () => useContext(SidebarDrawerContext)
