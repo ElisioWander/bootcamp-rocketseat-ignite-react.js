dab8aadd14ed7da86bab8def5fc6d0e3fae1908f
ElisioWander <elisio741@hotmail.com>
2022-01-03 20:30:11 -0300

    ActiveLink

--- react/dashgo/src/pages/users/create.tsx
+++ react/dashgo/src/pages/users/create.tsx
@@ -1,7 +1,8 @@
 import { Box, Flex, Heading, Divider, VStack, SimpleGrid, Button, HStack } from '@chakra-ui/react'
+import Link from 'next/link'
 import { Input } from '../../Components/Form/Input'
-import { Header } from '../../Components/Header'
-import { Sidebar } from '../../Components/Sidebar'
+import { Header } from '../../Components/Header/Index'
+import { Sidebar } from '../../Components/Sidebar/Index'
 
 export default function CreateUser() {
   return (
@@ -11,18 +12,18 @@ export default function CreateUser() {
       <Flex width="100%" my="6" maxWidth={1480} mx="auto" px="6" >
         <Sidebar />
 
-        <Box flex="1" borderRadius="8" bg="gray.800" p="8" >
+        <Box flex="1" borderRadius="8" bg="gray.800" p={["6", "8"]} >
           <Heading fontSize="lg" fontWeight="normal" >Criar Usuário</Heading>
 
           <Divider my="6" borderColor="gray.700" />
 
           <VStack spacing="8" >
-            <SimpleGrid minChildWidth="240px" spacing="8" width="100%" >
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} width="100%" >
               <Input name="name" label="Nome Completo" />
               <Input name="email" type="email" label="elisio741@hotmail.com" />
             </SimpleGrid>
 
-            <SimpleGrid minChildWidth="240px" spacing="8" width="100%" >
+            <SimpleGrid minChildWidth="240px" spacing={["6", "8"]} width="100%" >
               <Input name="password" type="password" label="Senha" />
               <Input name="password_confirmation" type="password_confirmation" label="Confirme a senha" />
             </SimpleGrid>
@@ -30,7 +31,9 @@ export default function CreateUser() {
 
           <Flex mt="8" justify="flex-end" >
             <HStack spacing="4" >
-              <Button colorScheme="whiteAlpha" >Cancelar</Button>
+              <Link href="/users" passHref >
+                <Button as="a" colorScheme="whiteAlpha" >Cancelar</Button>
+              </Link>
               <Button colorScheme="pink" >Salvar</Button>
             </HStack>
           </Flex>
--- react/dashgo/src/pages/users/index.tsx
+++ react/dashgo/src/pages/users/index.tsx
@@ -1,10 +1,16 @@
-import { Text, Box, Flex, Heading, Button, Icon, Table, Thead, Tbody, Tr, Th, Td, Checkbox } from '@chakra-ui/react'
-import { RiAddLine, RiPencilLine } from 'react-icons/ri'
+import { Text, Box, Flex, Heading, Button, Icon, Table, Thead, Tbody, Tr, Th, Td, Checkbox, useBreakpointValue } from '@chakra-ui/react'
+import Link from 'next/link'
+import { RiAddLine } from 'react-icons/ri'
 import { Header } from '../../Components/Header/Index'
 import { Pagination } from '../../Components/Pagination/Index'
 import { Sidebar } from '../../Components/Sidebar/Index'
 
 export default function UserList() {
+  const wideVersion = useBreakpointValue({
+    base: false,
+    lg: true
+  })
+
   return (
     <Box>
       <Header />
@@ -15,33 +21,34 @@ export default function UserList() {
         <Box flex="1" borderRadius="8" bg="gray.800" p="8" >
           <Flex mb="8" justify="space-between" align="center" >
             <Heading size="lg" fontWeight="normal" >Usuários</Heading>
-
-            <Button 
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-            >
-              Criar Novo
-            </Button>
+            <Link href="/users/create" passHref >
+              <Button 
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+              >
+                Criar Novo
+              </Button>
+            </Link>
 
           </Flex>
 
           <Table colorScheme="whiteAlpha" >
             <Thead>
               <Tr>
-                <Th px="6" color="gray.300" width="8" >
+                <Th px={["4", "4", "6"]} color="gray.300" width="8" >
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuários</Th>
-                <Th>Data de cadastro</Th>
+                { wideVersion &&  <Th>Data de cadastro</Th> }
                 <Th width="8" ></Th>
               </Tr>
             </Thead>
             <Tbody>
               <Tr>
-                <Td px="6" >
+                <Td px={["4", "4", "6"]} >
                   <Checkbox  colorScheme="pink" />
                 </Td>
                 <Td>
@@ -50,21 +57,35 @@ export default function UserList() {
                     <Text fontSize="sm" color="gray.300" >Elisio741@hotmail.com</Text>
                   </Box>
                 </Td>
-                <Td>05 Dezembro, 2021</Td>
-                <Td>
-                  <Button 
-                    as="a"
-                    size="sm"
-                    fontSize="sm"
-                    colorScheme="purple"
-                    leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
-                    _hover={{
-                      cursor: "pointer"
-                    }}
-                  >
-                    Criar Novo
-                  </Button>
+                { wideVersion && <Td>05 Dezembro, 2021</Td> }
+              </Tr>
+            </Tbody>
+            <Tbody>
+              <Tr>
+                <Td px={["4", "4", "6"]} >
+                  <Checkbox  colorScheme="pink" />
                 </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold" >Elisio Wander</Text>
+                    <Text fontSize="sm" color="gray.300" >Elisio741@hotmail.com</Text>
+                  </Box>
+                </Td>
+                { wideVersion && <Td>05 Dezembro, 2021</Td> }
+              </Tr>
+            </Tbody>
+            <Tbody>
+              <Tr>
+                <Td px={["4", "4", "6"]} >
+                  <Checkbox  colorScheme="pink" />
+                </Td>
+                <Td>
+                  <Box>
+                    <Text fontWeight="bold" >Elisio Wander</Text>
+                    <Text fontSize="sm" color="gray.300" >Elisio741@hotmail.com</Text>
+                  </Box>
+                </Td>
+                { wideVersion && <Td>05 Dezembro, 2021</Td> }
               </Tr>
             </Tbody>
 
