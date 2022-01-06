a0b4c770574ffee79f36b29cec825698cbf9d309
ElisioWander <elisio741@hotmail.com>
2022-01-06 20:01:20 -0300

    schema-validation-sign-in-page

--- react/dashgo/src/pages/index.tsx
+++ react/dashgo/src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { SubmitHandler, useForm } from "react-hook-form";
+import * as yup from "yup"
+import { yupResolver } from "@hookform/resolvers/yup"
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { Input } from "../Components/Form/Input";
 
@@ -7,14 +9,25 @@ type SignInFormData = {
   password: string;
 }
 
+const schema = yup.object({
+  email: yup.string().email('E-mail inválido').required('E-mail obrigatório'),
+  password: yup.string().required('Senha obrigatória')
+})
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm()
+  const { register,handleSubmit, formState } = useForm({
+    resolver: yupResolver(schema)
+  })
+
+  const { errors } = formState
 
   const handleSignIn: SubmitHandler<SignInFormData> = async (values) => {
     await new Promise(resolve => setTimeout(resolve, 2000))
 
     console.log(values)
   }
+
+  console.log(errors)
 
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
@@ -29,8 +42,22 @@ export default function SignIn() {
         onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" type="email" label="E-mail" {...register("email")} />
-          <Input name="password" type="password" label="Senha" {...register("password")} />
+          <Input 
+            name="email"
+            type="email"
+            label="E-mail" {...register("email")}
+            error={errors.email}
+            //dentro de errors existe outras mensagens, por isso é necessários
+            //especificar que você quer o que está dentro de email, nesse caso
+            />
+          <Input
+            name="password"
+            type="password"
+            label="Senha" {...register("password")}
+            error={errors.password}
+            //dentro de errors existe outras mensagens, por isso é necessários
+            //especificar que você quer o que está dentro de password, nesse caso
+            />
         </Stack>
 
         <Button type="submit" mt="6" colorScheme="pink" isLoading={formState.isSubmitting} >
